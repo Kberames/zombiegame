79065e21ea660f912713fe8f88a5d5b78573e2aa
Kimberly Ames <ameskimberly@gmail.com>
2016-11-06 12:15:24 -0700

    add initial files

--- app.js
+++ app.js
@@ -1,33 +1,167 @@
-var Fruit = (function () {
-    function Fruit(name, price, color) {
-        this.name = name;
-        this.price = price;
-        this.color = color;
+var canvas = document.getElementById('gameCanvas');
+var context = canvas.getContext("2d");
+var playButton = document.getElementById("playBtn");
+playButton.style.visibility = "hidden";
+var bgReady = false;
+var bgImage = new Image();
+bgImage.onload = function () {
+    bgReady = true;
+};
+bgImage.src = "images/brick.jpg";
+var deathReady = false;
+var deathImage = new Image();
+deathImage.onload = function () {
+    deathReady = true;
+};
+deathImage.src = "images/skull.png";
+var dead = false;
+var guyReady = false;
+var guyImage = new Image();
+guyImage.onload = function () {
+    guyReady = true;
+};
+guyImage.src = "images/guy.png";
+var zombieReady = false;
+var zombieImage = new Image();
+zombieImage.onload = function () {
+    zombieReady = true;
+};
+zombieImage.src = "images/zombie.png";
+var guy = {
+    speed: 256,
+    x: 0,
+    y: 0,
+    width: 50,
+    height: 75
+};
+var zombie = {
+    speed: 64,
+    x: 0,
+    y: 0,
+    width: 50,
+    height: 75
+};
+var zombiesCaught = 0;
+var bottom = canvas.height - zombie.height;
+var right = canvas.width - zombie.width;
+var corners = [
+    { x: 0, y: 0 },
+    { x: 0, y: bottom },
+    { x: right, y: 0 },
+    { x: right, y: bottom }
+];
+var keysDown = {};
+addEventListener("keydown", function (e) {
+    keysDown[e.keyCode] = true;
+}, false);
+addEventListener("keyup", function (e) {
+    delete keysDown[e.keyCode];
+}, false);
+playButton.addEventListener("click", function () {
+    playButton.style.visibility = "hidden";
+    dead = false;
+    then = Date.now();
+    reset();
+    mainloop();
+});
+var reset = function () {
+    guy.x = canvas.width / 2 - guy.width / 2;
+    guy.y = canvas.height / 2 - guy.height / 2;
+    var randomCorner = Math.floor(Math.random() * 4);
+    zombie.x = corners[randomCorner].x;
+    zombie.y = corners[randomCorner].y;
+};
+var update = function (modifier) {
+    if (38 in keysDown) {
+        guy.y -= guy.speed * modifier;
     }
-    return Fruit;
-}());
-var Telephone = (function () {
-    function Telephone(name, price, loudness) {
-        this.name = name;
-        this.price = price;
-        this.loudness = loudness;
+    if (40 in keysDown) {
+        guy.y += guy.speed * modifier;
     }
-    return Telephone;
-}());
-var fruit1 = new Fruit("orange", .50, "orange");
-var fruit2 = new Fruit("apple", .75, "red");
-var fruit3 = new Fruit("lime", 1, "green");
-var phone1 = new Telephone("iPhone", 699, 7);
-var phone2 = new Telephone("android", 649, 8);
-var inventory = [];
-inventory[0] = fruit1;
-inventory[1] = fruit2;
-inventory[2] = fruit3;
-inventory[3] = phone1;
-inventory[4] = phone2;
-function showInventory(product) {
-    console.log("Product: " + product.name + " Price: " + product.price);
-}
-for (var i = 0; i < inventory.length; i++) {
-    showInventory(inventory[i]);
-}
+    if (37 in keysDown) {
+        guy.x -= guy.speed * modifier;
+    }
+    if (39 in keysDown) {
+        guy.x += guy.speed * modifier;
+    }
+    if (guy.x < 0) {
+        guy.x = 0;
+    }
+    else if (guy.x > right) {
+        guy.x = right;
+    }
+    if (guy.y < 0) {
+        guy.y = 0;
+    }
+    else if (guy.y > bottom) {
+        guy.y = bottom;
+    }
+    var xDiff = guy.x - zombie.x;
+    var yDiff = guy.y - zombie.y;
+    if (xDiff > 0) {
+        zombie.x += zombie.speed * modifier;
+    }
+    else if (xDiff < 0) {
+        zombie.x -= zombie.speed * modifier;
+    }
+    if (yDiff > 0) {
+        zombie.y += zombie.speed * modifier;
+    }
+    else if (yDiff < 0) {
+        zombie.y -= zombie.speed * modifier;
+    }
+    if (guy.x <= (zombie.x + zombie.width)
+        && zombie.x <= (guy.x + guy.width)
+        && guy.y <= (zombie.y + zombie.height)
+        && zombie.y <= (guy.y + guy.height)) {
+        dead = true;
+        context.drawImage(deathImage, 0, 0, canvas.width, canvas.height);
+        context.font = '40pt Calibri';
+        context.fillStyle = 'red';
+        context.fillText('You\'re dead!', 250, 400);
+    }
+};
+var render = function () {
+    if (dead) {
+        if (deathReady) {
+            context.drawImage(deathImage, 0, 0, canvas.width, canvas.height);
+            context.font = '40pt Calibri';
+            context.fillStyle = 'red';
+            context.fillText('You\'re dead!', 250, 400);
+        }
+    }
+    else {
+        if (bgReady) {
+            context.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
+        }
+        if (guyReady) {
+            context.drawImage(guyImage, guy.x, guy.y, guy.width, guy.height);
+        }
+        if (zombieReady) {
+            context.drawImage(zombieImage, zombie.x, zombie.y, zombie.width, zombie.height);
+        }
+    }
+};
+var mainloop = function () {
+    var now = Date.now();
+    var delta = now - then;
+    update(delta / 1000);
+    render();
+    if (!dead) {
+        then = now;
+        animFrame(mainloop);
+    }
+    else {
+        playButton.style.visibility = "visible";
+    }
+};
+var w = window;
+var animFrame = window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    null;
+var then = Date.now();
+reset();
+mainloop();
--- app.ts
+++ app.ts
@@ -1,34 +1,217 @@
-interface IProduct {
-    name: string;
-    price: number;
-}
+// create canvas
+let canvas = <HTMLCanvasElement>document.getElementById('gameCanvas');
+let context = canvas.getContext("2d");
 
-class Fruit implements IProduct {
-    constructor(public name: string, public price: number, public color: string) { }
-}
+let playButton = document.getElementById("playBtn");
+playButton.style.visibility = "hidden";
 
-class Telephone implements IProduct {
-    constructor(public name: string, public price: number, public loudness: number) { }
-}
+// Background image
+let bgReady = false;
+let bgImage = new Image();
+bgImage.onload = function () {
+	bgReady = true;
+};
+bgImage.src = "images/brick.jpg";
 
-let fruit1 = new Fruit("orange", .50, "orange");
-let fruit2 = new Fruit("apple", .75, "red");
-let fruit3 = new Fruit("lime", 1, "green");
+// Death image
+let deathReady = false;
+let deathImage = new Image();
+deathImage.onload = function () {
+	deathReady = true;
+};
+deathImage.src = "images/skull.png";
 
-let phone1 = new Telephone("iPhone", 699, 7);
-let phone2 = new Telephone("android", 649, 8);
+let dead = false;
 
-let inventory = [];
-inventory[0] = fruit1;
-inventory[1] = fruit2;
-inventory[2] = fruit3;
-inventory[3] = phone1;
-inventory[4] = phone2;
+// Guy image
+let guyReady = false;
+let guyImage = new Image();
+guyImage.onload = function () {
+	guyReady = true;
+};
+guyImage.src = "images/guy.png";
 
-function showInventory(product: IProduct) {
-    console.log("Product: " + product.name + " Price: " + product.price);
-}
+// Zombie image
+let zombieReady = false;
+let zombieImage = new Image();
+zombieImage.onload = function () {
+	zombieReady = true;
+};
+zombieImage.src = "images/zombie.png";
 
-for (let i=0; i < inventory.length; i++){
-  showInventory(inventory[i]);
-}
+// Game objects
+let guy = {
+	speed: 256, // movement in pixels per second
+	x: 0,
+	y: 0,
+  width: 50,
+  height: 75
+};
+let zombie = {
+  speed: 64, // movement in pixels per second
+  x: 0,
+	y: 0,
+  width: 50,
+  height: 75
+};
+let zombiesCaught = 0;
+
+let bottom = canvas.height - zombie.height;
+let right = canvas.width - zombie.width;
+
+
+let corners = [
+  {x: 0, y: 0},
+  {x: 0, y: bottom},
+  {x: right, y: 0},
+  {x: right, y: bottom}
+];
+
+// Handle keyboard controls
+let keysDown = {};
+
+addEventListener("keydown", function (e) {
+	keysDown[e.keyCode] = true;
+}, false);
+
+addEventListener("keyup", function (e) {
+	delete keysDown[e.keyCode];
+}, false);
+
+playButton.addEventListener("click", function(){
+  playButton.style.visibility = "hidden";
+  dead = false;
+  then = Date.now();
+  reset();
+  mainloop();
+});
+
+// Reset the game
+let reset = function () {
+
+  //Place guy in the center
+	guy.x = canvas.width / 2 - guy.width / 2;
+	guy.y = canvas.height / 2 - guy.height / 2;
+
+	// Throw the zombie in a random corner
+  let randomCorner = Math.floor(Math.random() * 4);
+  zombie.x = corners[randomCorner].x;
+	zombie.y = corners[randomCorner].y;
+};
+
+// Update game objects
+let update = function (modifier) {
+	if (38 in keysDown) { // Player holding up
+		guy.y -= guy.speed * modifier;
+	}
+	if (40 in keysDown) { // Player holding down
+		guy.y += guy.speed * modifier;
+	}
+	if (37 in keysDown) { // Player holding left
+		guy.x -= guy.speed * modifier;
+	}
+	if (39 in keysDown) { // Player holding right
+		guy.x += guy.speed * modifier;
+	}
+
+  //keep guy on the screen
+  if (guy.x < 0){
+    guy.x = 0;
+  }
+  else if (guy.x > right){
+    guy.x = right;
+  }
+  if (guy.y < 0){
+    guy.y = 0;
+  }
+  else if (guy.y > bottom){
+    guy.y = bottom;
+  }
+
+  //move zombie in direction of guy
+  let xDiff = guy.x - zombie.x;
+  let yDiff = guy.y - zombie.y;
+
+  if(xDiff > 0){
+    zombie.x += zombie.speed * modifier;
+  }
+  else if(xDiff < 0){
+    zombie.x -= zombie.speed * modifier;
+  }
+  if(yDiff > 0){
+    zombie.y += zombie.speed * modifier;
+  }
+  else if(yDiff < 0){
+    zombie.y -= zombie.speed * modifier;
+  }
+
+	// Are they touching?
+	if (
+		guy.x <= (zombie.x + zombie.width)
+		&& zombie.x <= (guy.x + guy.width)
+		&& guy.y <= (zombie.y + zombie.height)
+		&& zombie.y <= (guy.y + guy.height)) {
+    dead = true;
+    context.drawImage(deathImage, 0, 0, canvas.width, canvas.height);
+    context.font = '40pt Calibri';
+    context.fillStyle = 'red';
+    context.fillText('You\'re dead!', 250, 400);
+	}
+};
+
+// Draw everything
+let render = function () {
+  if(dead){
+    if (deathReady) {
+      context.drawImage(deathImage, 0, 0, canvas.width, canvas.height);
+      context.font = '40pt Calibri';
+      context.fillStyle = 'red';
+      context.fillText('You\'re dead!', 250, 400);
+     }
+  }
+  else{
+  	if (bgReady) {
+  		context.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
+  	}
+
+  	if (guyReady) {
+  		context.drawImage(guyImage, guy.x, guy.y, guy.width, guy.height);
+  	}
+
+  	if (zombieReady) {
+  		context.drawImage(zombieImage, zombie.x, zombie.y, zombie.width, zombie.height);
+  	}
+  }
+};
+
+// The main game loop
+let mainloop = function () {
+	let now = Date.now();
+	let delta = now - then;
+
+	update(delta / 1000);
+	render();
+
+  if(!dead){
+    then = now;
+  	// Request to do this again ASAP
+  	animFrame(mainloop);
+  }
+  else{
+    playButton.style.visibility = "visible";
+  }
+};
+
+// Cross-browser support for requestAnimationFrame
+let w = window;
+let animFrame = window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        window.oRequestAnimationFrame      ||
+        window.msRequestAnimationFrame     ||
+        null ;
+
+// Let's play this game!
+let then = Date.now();
+reset();
+mainloop();
